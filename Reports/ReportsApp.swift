5d5516e45d14e91cdf0a154a1f0d1381bc171c12
Daniel Amoafo <danielamoafo+github@gmail.com>
2024-02-03 18:01:20 +1100

    Initial Commit

---
+++ Reports/ReportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReportsApp.swift
+//  Reports
+//
+//  Created by Daniel Amoafo on 3/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
